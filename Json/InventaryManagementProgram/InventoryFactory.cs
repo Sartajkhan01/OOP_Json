e3d8ab4e4be36fa688061f19392cc9b0aac0bf50
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2023-01-14 22:31:45 +0530

    [Sartaj]: Json inventory data management program

---
+++ Json/InventaryManagementProgram/InventoryFactory.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json.InventaryManagementProgram
+{
+    public class InventoryFactory
+    {
+        public InventoryDetails ReadJson(string filePath)
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                var json = reader.ReadToEnd();
+                var inventory = JsonConvert.DeserializeObject<InventoryDetails>(json);
+                return inventory;
+            }
+        }
+        public void WriteToJson(string filePath, InventoryDetails details)
+        {
+            var json = JsonConvert.SerializeObject(details);
+            File.WriteAllText(filePath, json);
+        }
+    }
+}
